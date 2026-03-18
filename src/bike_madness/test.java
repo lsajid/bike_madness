bc7a4eecea7ccaedb91fd64910050be773e2f765
Alphons Saji <alphonsperakattu@gmail.com>
2026-03-18 10:46:47 -0400

    Title screen updated

--- src/bike_madness/test.java
+++
@@ -1,9 +0,0 @@
-package bike_madness;
-
-public class test {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-	}
-}
--- src/src/Rect.java
+++ src/src/Rect.java
@@ -1,4 +1,4 @@
-package src;
+
 import java.awt.Graphics;
 public class Rect {
     int x;
@@ -33,4 +33,11 @@ public class Rect {
         x += dx;
         y += dy;
     }
+    
+	public boolean contains(int mx, int my) {
+		return (mx > x)      && 
+			   (my > y)      && 
+			   (mx < x + this.width)  && 
+			   (my < y + this.height);
+	}
 }
